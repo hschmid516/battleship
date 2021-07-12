5be5241fb41afa392e5e13bff516bde39c4fd759
ChloeLL <chloell@protonmail.ch>
2021-07-12 15:11:05 -0700

    Add Health and Sunk Methods

--- lib/ship.rb
+++ lib/ship.rb
@@ -2,10 +2,17 @@ class Ship
   attr_reader :name,
               :length
 
-
   def initialize(name, length)
     @name   = name
     @length = length
     @health = length
+  end
+
+  def health
+    @health
+  end
+
+  def sunk?
+    @health < 1
   end
 end
