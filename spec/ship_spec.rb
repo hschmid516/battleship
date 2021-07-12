bd2fc921297c10d4351005856b094392a557ce20
ChloeLL <chloell@protonmail.ch>
2021-07-12 15:11:45 -0700

    Add Parameter and Sunk Tests

--- spec/ship_spec.rb
+++ spec/ship_spec.rb
@@ -6,4 +6,28 @@ RSpec.describe Ship do
 
     expect(cruiser).to be_a(Ship)
   end
+
+  it 'has a name' do
+    cruiser = Ship.new("Cruiser", 3)
+
+    expect(cruiser.name).to eq("Cruiser")
+  end
+
+  it 'displays length' do
+    cruiser = Ship.new("Cruiser", 3)
+
+    expect(cruiser.length).to eq(3)
+  end
+
+  it 'displays health' do
+    cruiser = Ship.new("Cruiser", 3)
+
+    expect(cruiser.health).to eq(3)
+  end
+
+  it 'is not sunk' do
+    cruiser = Ship.new("Cruiser", 3)
+
+    expect(cruiser.sunk?).to be false
+  end
 end
