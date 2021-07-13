26903a18e90315330772f891b824b6b6837e3920
ChloeLL <chloell@protonmail.ch>
2021-07-13 16:08:26 -0700

    Create render method tests

--- spec/cell_spec.rb
+++ spec/cell_spec.rb
@@ -87,7 +87,7 @@ RSpec.describe Cell do
     it 'renders as . by default' do
       cell_1 = Cell.new('B4')
 
-      expect(cell_1.render).to eq(".")
+      expect(cell_1.render).to eq('.')
     end
 
     it 'renders as M if fired upon and no ship' do
@@ -95,7 +95,7 @@ RSpec.describe Cell do
 
       cell_1.fire_upon
 
-      expect(cell_1.render).to eq("M")
+      expect(cell_1.render).to eq('M')
     end
 
     it 'renders as . if not fired upon' do
@@ -103,23 +103,37 @@ RSpec.describe Cell do
       cruiser = Ship.new('Cruiser', 3)
       cell_2.place_ship(cruiser)
 
-      expect(cell_2.render).to eq(".")
+      expect(cell_2.render).to eq('.')
     end
 
-    xit 'renders as S if not fired upon' do
+    it 'renders as S if not fired upon and show_ship is true' do
       cell_2 = Cell.new('C3')
       cruiser = Ship.new('Cruiser', 3)
       cell_2.place_ship(cruiser)
 
-      expect(cell_2.render(true)).to eq("S")
+      expect(cell_2.render(true)).to eq('S')
     end
 
-    xit 'renders as . if not fired upon' do
+    it 'renders as H if ship is hit' do
       cell_2 = Cell.new('C3')
       cruiser = Ship.new('Cruiser', 3)
       cell_2.place_ship(cruiser)
+      cell_2.fire_upon
+      cruiser.sunk?
 
-      expect(cell_2.render).to eq(".")
+      expect(cell_2.render).to eq('H')
+    end
+
+    it 'renders as X if ship is sunk' do
+      cell_2 = Cell.new('C3')
+      cruiser = Ship.new('Cruiser', 3)
+      cell_2.place_ship(cruiser)
+      cell_2.fire_upon
+      cruiser.hit
+      cruiser.hit
+      cruiser.sunk?
+
+      expect(cell_2.render).to eq('X')
     end
   end
 end
