8c62f034b6bf399f2bbe700333ce03dc5df3f2e9
Henry Schmid <henry.schmid1@gmail.com>
2021-07-13 19:16:20 -0600

    Create cells method and test

--- spec/board_spec.rb
+++ spec/board_spec.rb
@@ -1,4 +1,5 @@
 require './lib/board'
+require './lib/cell'
 
 RSpec.describe Board do
   context 'Creating the board' do
@@ -6,6 +7,15 @@ RSpec.describe Board do
       board = Board.new
 
       expect(board).to be_a(Board)
+    end
+
+    it 'creates a board' do
+      board = Board.new
+
+      expect(board.cells).to be_a(Hash)
+      expect(board.cells.keys.length).to eq(16)
+      expect(board.cells.values.length).to eq(16)
+      expect(board.cells.values.first).to be_a(Cell)
     end
   end
 end
