1221c39c259204fd3827275a4a7f1a13cab48b7a
ChloeLL <chloell@protonmail.ch>
2021-07-15 09:57:45 -0700

    Refactor code

---
+++ lib/board.rb
@@ -0,0 +1,116 @@
+class Board
+  attr_reader :ship,
+              :cells
+
+  def initialize
+    @ship  = ship
+    @cells = cell_grid = {}
+    size = 4
+    num = 1..size
+    ltr = 65.chr..(64 + size).chr
+    num.map do |num|
+      ltr.map do |ltr|
+        coord = ltr + num.to_s
+        cell_grid[coord] = Cell.new(coord)
+      end
+    end
+    cell_grid
+  end
+
+  def valid_coordinate?(coord)
+    @cells.keys.to_s.include?(coord) == true
+  end
+
+  def letters_consecutive?(coords)
+    letter_range = []
+
+    letters = coords.map do |coord|
+      coord[0]
+    end
+
+    ('A'..'D').each_cons(coords.length) do |letter|
+      letter_range << letter
+    end
+
+    letter_range.any? do |letter|
+      letter == letters
+    end
+  end
+
+  def numbers_consecutive?(coords)
+    num_range = []
+
+    numbers = coords.map do |coord|
+      coord[1].to_i
+    end
+
+    (1..4).each_cons(coords.length) do |num|
+      num_range << num
+    end
+
+    num_range.any? do |num|
+      num == numbers
+    end
+  end
+
+  def same_letters?(coords)
+    coords.all? do |coord|
+      coord[0] == coords[0][0]
+    end
+  end
+
+  def same_numbers?(coords)
+    coords.all? do |coord|
+      coord[1] == coords[0][1]
+    end
+  end
+
+  def length_valid?(ship, coords)
+    ship.length == coords.length
+  end
+
+  def valid_placement?(ship, coords)
+    length_valid?(ship, coords) && numbers_consecutive?(coords) &&
+      same_letters?(coords) && !ship_overlap?(coords) ||
+      length_valid?(ship, coords) && letters_consecutive?(coords) &&
+        same_numbers?(coords) && !ship_overlap?(coords)
+  end
+
+  def place(ship, coords)
+    coords.map do |coord|
+      @cells[coord].place_ship(ship)
+    end
+  end
+
+  def ship_overlap?(coords)
+    coords.any? do |coord|
+      !@cells[coord].ship.nil?
+    end
+
+    def render
+      i = 1
+      size = 4
+      print ' '
+      loop do
+        print " #{i}"
+        i += 1
+        break if i > size
+      end
+      print "\n"
+      j = 0
+      loop do
+        print "#{(65 + j).chr} "
+        k = 1
+        loop do
+          print "#{@cells[(65 + j).chr + k.to_s].render} "
+          k += 1
+          break if k > size
+        end
+        print "\n"
+        j += 1
+        break if j >= size
+      end
+      print "\n"
+    end
+  end
+end
---
+++ spec/board_spec.rb
@@ -0,0 +1,107 @@
+
+require './lib/board'
+require './lib/ship'
+require './lib/cell'
+
+RSpec.describe Board do
+  context 'Creating the board' do
+    it 'exists' do
+      board = Board.new
+
+      expect(board).to be_a(Board)
+    end
+
+    it 'creates a board' do
+      board = Board.new
+
+      expect(board.cells).to be_a(Hash)
+      expect(board.cells.keys.length).to eq(16)
+      expect(board.cells.values.length).to eq(16)
+      expect(board.cells.values.first).to be_a(Cell)
+    end
+  end
+
+  context 'Creating the board' do
+    it 'can validate coordinates' do
+      board = Board.new
+      board.cells
+
+      expect(board.valid_coordinate?("A1")).to be true
+      expect(board.valid_coordinate?("D4")).to be true
+      expect(board.valid_coordinate?("A5")).to be false
+      expect(board.valid_coordinate?("E1")).to be false
+      expect(board.valid_coordinate?("A22")).to be false
+    end
+
+    it 'confirms coordinates = ship length' do
+      board = Board.new
+      cruiser = Ship.new("Cruiser", 3)
+      submarine = Ship.new("Submarine", 2)
+
+      expect(board.valid_placement?(cruiser, ["A1", "A2"])).to be false
+      expect(board.valid_placement?(submarine, ["A2", "A3", "A4"])).to be false
+      # expect(board.valid_placement?(submarine, ["A2", "A3"])).to be true
+    end
+
+    xit 'confirms coordinates are consecutive'do
+      board = Board.new
+      cruiser = Ship.new("Cruiser", 3)
+      submarine = Ship.new("Submarine", 2)
+
+      expect(board.valid_placement?(cruiser, ["A1", "A2", "A4"])).to be false
+      expect(board.valid_placement?(submarine, ["A1", "C1"])).to be false
+      expect(board.valid_placement?(cruiser, ["A3", "A2", "A1"])).to be false
+      expect(board.valid_placement?(submarine, ["C1", "B1"])).to be false
+      expect(board.valid_placement?(cruiser, ["A1", "B2", "C3"])).to be false
+      expect(board.valid_placement?(submarine, ["C2", "D3"])).to be false
+      expect(board.valid_placement?(submarine, ["A1", "A2"])).to be true
+      expect(board.valid_placement?(cruiser, ["B1", "C1", "D1"])).to be true
+    end
+    end
+
+  context 'Placing ships' do
+    xit 'shows a ship is placed on multiple cells' do
+      board = Board.new
+      cruiser = Ship.new("Cruiser", 3)
+
+      board.place(cruiser, ["A1", "A2", "A3"])
+      cell_1 = board.cells["A1"]
+      cell_2 = board.cells["A2"]
+      cell_3 = board.cells["A3"]
+
+      expect(cell_1.ship).to eq(cruiser)
+      expect(cell_2.ship).to eq(cruiser)
+      expect(cell_3.ship).to eq(cruiser)
+      expect(cell_3.ship == cell_2.ship).to be true
+    end
+
+    it 'verifies ships are not overlapping' do
+      board = Board.new
+      cruiser = Ship.new("Cruiser", 3)
+
+      board.place(cruiser, ["A1", "A2", "A3"])
+      submarine = Ship.new("Submarine", 2)
+
+      expect(board.valid_placement?(submarine, ["A1", "B1"])).to be false
+    end
+  end
+
+  context 'Rendering the board' do
+    it 'renders' do
+      board = Board.new
+      require 'pry'; binding.pry
+      # cruiser = Ship.new('Cruiser', 3)
+      # board.place(cruiser, %w[A1 A2 A3])
+
+      expect(board.render).to eq("  1 2 3 4 \nA . . . . \nB . . . . \nC . . . . \nD . . . . \n")
+    end
+
+    xit 'renders and shows a ship' do
+      board = Board.new
+      cruiser = Ship.new('Cruiser', 3)
+      board.place(cruiser, %w[A1 A2 A3])
+
+      expect(board.render(true))
+    end
+  end
+end
