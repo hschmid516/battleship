a993c2049f210cea4905d94cfdd461fd73f4b125
ChloeLL <chloell@protonmail.ch>
2021-07-15 23:07:21 -0700

    Add multiline coord input

--- lib/player.rb
+++ lib/player.rb
@@ -10,12 +10,19 @@ class Player
 
   def player_ships
     puts "Enter the squares for the Cruiser (3 spaces)"
-    ship_square = gets.upcase
+    ship_square = gets.strip.upcase
+    puts "Square 1: #{ship_square}. Enter Square 2"
+    ship_square = gets.strip.upcase
+    puts "Square 2: #{ship_square}. Enter Square 3"
+    ship_square = gets.strip.upcase
+    puts "Square 3: #{ship_square}."
     # Verify and place
     # If not valid, redo
     # show board, render true
     puts "Enter the squares for the Submarine (2 spaces)"
-    ship_square = gets.upcase
+    ship_square = gets.strip.upcase
+    puts "Square 1: #{ship_square}. Enter Square 2"
+    ship_square = gets.strip.upcase
     # Verify and place
     # If not valid, redo
     # show board, render true
