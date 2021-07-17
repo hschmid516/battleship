0216b93aee609846bdc21f3de15524d4e931cda2
Henry Schmid <henry.schmid1@gmail.com>
2021-07-17 10:49:22 -0600

    Refactor code

--- lib/game.rb
+++ lib/game.rb
@@ -40,21 +40,59 @@ class Game
     STDIN.getch
   end
 
+  def print_slow(text)
+    text.split(//).each do |char|
+      sleep 0.07
+      print char
+    end
+  end
+
+  def print_fast(text)
+    text.split(//).each do |char|
+      sleep 0.005
+      print char
+    end
+  end
+
+  def play_quit
+    input = gets.strip.downcase
+
+    while input != 'p' && input != 'q'
+      puts "\nPlease enter p or q"
+      input = gets.strip.downcase
+    end
+
+    if input == 'q'
+      abort "You may have lost the battle, but you also lost the war!"
+    end
+
+    system "clear"
+    system "cls"
+  end
+
+  def play_turns(board_size)
+    com = Computer.new(board_size)
+    com.com_placement
+    com.com_speaks
+    # puts display_boards(com)
+
+    player = Player.new(board_size)
+    player.player_ships
+    puts display_boards(com, player)
+    player.turns(com)
+    com.turns(player)
+    hit_check(com, player)
+    puts display_boards(com, player)
+
+    while win_condition(player, com) == false
+      player.turns(com)
+      com.turns(player)
+      hit_check(com, player)
+      puts display_boards(com, player)
+    end
+  end
+
   def play
-    def print_slow(text)
-      text.split(//).each do |char|
-        sleep 0.07
-        print char
-      end
-    end
-
-    def print_fast(text)
-      text.split(//).each do |char|
-        sleep 0.005
-        print char
-      end
-    end
-
     system "clear"
     system "cls"
     print "                        "
@@ -69,66 +107,33 @@ class Game
     ")
     puts "\n\n                        Enter p to play. Enter q to quit."
 
-    play_quit = gets.strip.downcase
+    play_quit
 
-    while play_quit != 'p' && play_quit != 'q'
-      puts "\nPlease enter p or q"
-      play_quit = gets.strip.downcase
-    end
-
-    if play_quit == 'q'
-      abort "You may have lost the battle, but you also lost the war!"
-    end
-
-    if play_quit == 'p'
-
-    system "clear"
-    system "cls"
     puts "Select game mode" +
          "\n\n[4]x4\n[T]raditional\n[C]ustom\n[Q]uit"
-    play_mode = gets.strip.downcase
-    end
+    game_mode = gets.strip.downcase
 
-    while play_mode != '4' && play_mode != 't' && play_mode != 'c' && play_mode != 'q'
+
+    while game_mode != '4' && game_mode != 't' && game_mode !=
+       'c' && game_mode != 'q'
       puts "\n Please enter 4, t, c, or q"
 
-      play_mode = gets.strip.downcase
+      game_mode = gets.strip.downcase
     end
 
-    if play_mode == '4'
-
-      com = Computer.new(4)
-      com.com_placement
-      com.com_speaks
-      # puts display_boards(com)
-
-      player = Player.new
-      player.player_ships
-
-      puts display_boards(com, player)
-
-      player.turns(com)
-      com.turns(player)
-      hit_check(com, player)
-      puts display_boards(com, player)
-      while win_condition(player, com) == false
-        player.turns(com)
-        com.turns(player)
-        hit_check(com, player)
-        puts display_boards(com, player)
-      end
+    if game_mode == '4'
+      play_turns(4)
     end
 
-    if play_mode == 't'
-      com = Computer.new(10)
-      puts display_boards(com)
+    if game_mode == 't'
+      play_turns(10)
     end
 
-    if play_mode == 'c'
+    if game_mode == 'c'
       # We start the custom game from here
     end
 
-    if play_mode == 'q'
+    if game_mode == 'q'
       abort "You may have lost the battle, but you also lost the war!"
     end
   end
--- lib/player.rb
+++ lib/player.rb
@@ -7,98 +7,98 @@ class Player
               :submarine,
               :shot_square
 
-  def initialize
+  def initialize(board_size)
     @ship_square = []
-    @p_board     = Board.new(4)
+    @p_board     = Board.new(board_size)
     @cruiser     = cruiser
     @submarine   = submarine
     @shot_square = nil
+  end
 
-    def player_ships
+  def player_ships
+    puts 'Enter the squares for the Cruiser (3 spaces):'
+    @ship_square << gets.strip.upcase
+    print "Square 1: #{ship_square}. Enter Square 2:"
+    @ship_square << gets.strip.upcase
+    print "Square 2: #{ship_square}. Enter Square 3:"
+    @ship_square << gets.strip.upcase
+    puts "Square 3: #{ship_square}."
+    @cruiser = Ship.new('Cruiser', 3)
+    @p_board.valid_placement?(cruiser, @ship_square)
+
+    while @p_board.valid_placement?(cruiser, @ship_square) == false
+      @ship_square = []
+      puts 'Those are invalid coordinates. Please try again:'
       puts 'Enter the squares for the Cruiser (3 spaces):'
       @ship_square << gets.strip.upcase
-      puts "Square 1: #{ship_square}. Enter Square 2:"
+      print "Square 1: #{ship_square}. Enter Square 2:"
       @ship_square << gets.strip.upcase
-      puts "Square 2: #{ship_square}. Enter Square 3:"
+      print "Square 2: #{ship_square}. Enter Square 3:"
       @ship_square << gets.strip.upcase
       puts "Square 3: #{ship_square}."
-      @cruiser = Ship.new(cruiser, 3)
       @p_board.valid_placement?(cruiser, @ship_square)
+    end
 
-      while @p_board.valid_placement?(cruiser, @ship_square) == false
-        @ship_square = []
-        puts 'Those are invalid coordinates. Please try again:'
-        puts 'Enter the squares for the Cruiser (3 spaces):'
-        @ship_square << gets.strip.upcase
-        puts "Square 1: #{ship_square}. Enter Square 2:"
-        @ship_square << gets.strip.upcase
-        puts "Square 2: #{ship_square}. Enter Square 3:"
-        @ship_square << gets.strip.upcase
-        puts "Square 3: #{ship_square}."
-        @p_board.valid_placement?(cruiser, @ship_square)
-      end
+    @p_board.place(cruiser, @ship_square)
+    system "clear"
+    system "cls"
+    puts @p_board.render(true)
 
-      @p_board.place(cruiser, @ship_square)
-      system "clear"
-      system "cls"
-      puts @p_board.render(true)
+    @ship_square = []
+    puts 'Enter the squares for the Submarine (2 spaces):'
+    @ship_square << gets.strip.upcase
+    puts "Square 1: #{ship_square}. Enter Square 2:"
+    @ship_square << gets.strip.upcase
+    @submarine = Ship.new('Submarine', 2)
+    @p_board.valid_placement?(submarine, @ship_square)
 
+    while @p_board.valid_placement?(submarine, @ship_square) == false
       @ship_square = []
+      puts 'Those are invalid coordinates. Please try again:'
       puts 'Enter the squares for the Submarine (2 spaces):'
       @ship_square << gets.strip.upcase
       puts "Square 1: #{ship_square}. Enter Square 2:"
       @ship_square << gets.strip.upcase
-      @submarine = Ship.new(submarine, 2)
       @p_board.valid_placement?(submarine, @ship_square)
-
-      while @p_board.valid_placement?(submarine, @ship_square) == false
-        @ship_square = []
-        puts 'Those are invalid coordinates. Please try again:'
-        puts 'Enter the squares for the Submarine (2 spaces):'
-        @ship_square << gets.strip.upcase
-        puts "Square 1: #{ship_square}. Enter Square 2:"
-        @ship_square << gets.strip.upcase
-        @p_board.valid_placement?(submarine, @ship_square)
-      end
-
-      @p_board.place(submarine, @ship_square)
-
-      system 'clear'
-      system 'cls'
-      puts @p_board.render(true)
-      puts 'Ships placed. Press any key to start'
-      STDIN.getch
     end
 
-    def hit_check(com)
-      if com.com_board.cells[@shot_square].render == "X" ||
-          com.com_board.cells[@shot_square].render == "H"
-        puts "Your shot on #{shot_square} was a hit!"
-      else
-        puts "Your shot on #{shot_square} was a miss."
-      end
+    @p_board.place(submarine, @ship_square)
 
-      if com.com_board.cells[@shot_square].render == "X"
-        puts "You sunk a ship!"
-      end
+    system 'clear'
+    system 'cls'
+    puts @p_board.render(true)
+    puts 'Ships placed. Press any key to start'
+    STDIN.getch
+  end
+
+  def hit_check(com)
+    if com.com_board.cells[@shot_square].render == "X" ||
+        com.com_board.cells[@shot_square].render == "H"
+      puts "Your shot on #{shot_square} was a hit!"
+    else
+      puts "Your shot on #{shot_square} was a miss."
     end
 
-    def turns(com)
-      puts "Enter the coordinate for your shot:"
+    if com.com_board.cells[@shot_square].render == "X"
+      puts "You sunk a ship!"
+    end
+  end
+
+  def turns(com)
+    puts "Enter the coordinate for your shot:"
+    @shot_square = gets.strip.upcase
+    while com.com_board.valid_coordinate?(@shot_square) == false
+      puts "Please enter a valid coordinate:"
       @shot_square = gets.strip.upcase
-      while com.com_board.valid_coordinate?(@shot_square) == false
-        puts "Please enter a valid coordinate:"
-        @shot_square = gets.strip.upcase
-        com.com_board.valid_coordinate?(@shot_square)
-      end
-
-      while com.com_board.cells[@shot_square].fired_upon? == true
-        puts "You have already fired here. Try again:"
-        @shot_square = gets.strip.upcase
-      end
-
-      com.com_board.cells[@shot_square].fire_upon
-      com.com_board.cells[@shot_square].render
+      com.com_board.valid_coordinate?(@shot_square)
     end
+
+    while com.com_board.cells[@shot_square].fired_upon? == true
+      puts "You have already fired here. Try again:"
+      @shot_square = gets.strip.upcase
+    end
+
+    com.com_board.cells[@shot_square].fire_upon
+    com.com_board.cells[@shot_square].render
   end
 end
