b10bd026edc7142133f33424dc47e29dc3f3f850
ChloeLL <chloell@protonmail.ch>
2021-07-20 14:52:08 -0700

    Add player tests

---
+++ spec/player_spec.rb
@@ -0,0 +1,28 @@
+require './lib/game'
+require './lib/ship'
+require './lib/player'
+
+RSpec.describe Player do
+  it 'exists' do
+    player = Player.new(4)
+
+    expect(player).to be_a(Player)
+  end
+
+  it 'has a board' do
+    player = Player.new(4)
+
+    expect(player.p_board).to be_a(Board)
+  end
+
+  it 'creates ships' do
+    player = Player.new(4)
+    player.create_ships('trad')
+
+    expect(player.ships[1].name).to eq('Cruiser')
+    expect(player.ships[2].name).to eq('Submarine')
+    expect(player.ships[3].name).to eq('Destroyer')
+    expect(player.ships[4].name).to eq('Battleship')
+    expect(player.ships[5].name).to eq('Carrier')
+  end
+end
